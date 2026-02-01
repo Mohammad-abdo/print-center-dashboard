19dff1808ef2726c4892103ce018a78feee45b1c
Mohammad Abdlrahman Sad <91919000+Mohammad-abdo@users.noreply.github.com>
2026-02-01 17:30:28 +0200

    new

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:6008/api',
+  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:6008/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://back-studify.developteam.site/api',
 });
 
 // Add token if exists
